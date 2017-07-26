5a41bb9565236da50f6abe8cd4a559b9fcaaea0e
palakagarwal2016 <thepalakagarwal@gmail.com>
2017-07-26 14:36:54 -0700

    pls work part 1

---
+++ volunteer/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Amy and Palak are so cool! </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
